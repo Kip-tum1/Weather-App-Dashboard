c3937f07f051c650acbf44e2da7753be2861c69a
Kiptum <duncankipkoech12@gmail.com>
2025-10-04 14:29:45 +0300

    initial commit

---
+++ weather-app/src/components/Weather.jsx
@@ -0,0 +1,18 @@
+function Weather(){
+
+    return(
+        <div>
+            <h1>Weather-App Dashboard</h1>
+            <div>
+                <div>
+                    <input type="text" required/>
+                    <button>Search</button>                    
+                </div>
+                <h2>Mombasa</h2>
+                <h2>32 degrees celcious</h2>
+            </div>
+        </div>
+    )
+
+}
+export default Weather
